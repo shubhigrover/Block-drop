8b373750f41a1af51fa6362873410fe18de0d430
shubhigrover <78214886+shubhigrover@users.noreply.github.com>
2022-01-29 12:38:52 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Block-drop
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ block.js
@@ -0,0 +1,22 @@
+class block{
+    constructor(x,y,w,h){
+    var options={
+        isStatic:true,
+    
+    }
+    this.body=Bodies.rectangle(x,y,w,h,options);
+    this.w=w;
+    this.h=h;
+    World.add(world,this.body)
+    }
+    display(){
+    var pos= this.body.position;
+    var angle=this.body.angle;
+    push()
+    translate(pos.x,pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    rect(0,0,this.w,this.h)
+    pop()
+    }
+}
